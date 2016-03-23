2829036acbc1b23747065d0b5e167fda22f27055
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-03-23 18:04:32 +0800

    Add command src for updating cordova config.xml

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# cordova-ci-config
-Allows cordova's config.xml to be updated from the command line to set fields like app name and version which is useful in a continuous integration build environment.
+# cordova-update-config
+
+  A command line tool to update cordova's config.xml, useful in continuous integration build environments.
+
+Synopsis
+
+  cordova-update-config --appname org.company.app --appversion 5.3.223 ...<br/>
+  cordova-update-config --help
+
+Options
+
+| Alias | Argument            | Description                                  |
+| ---   | ---                 | ---                                          |
+| -h    | --help              | Display this usage guide.                    |
+| -v    | --version           | Display the version of this command          |
+| -n    | --appname string    | The application name to update config.xml    |
+| -a    | --appversion string | The application version to update config.xml |
+| -i    | --appid string      | The application id to update config.xml      |
---
+++ bin/cordova-update-config.js
@@ -0,0 +1,83 @@
+#! /usr/bin/env node
+
+var commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+  {
+    name: 'help', description: 'Display this usage guide.',
+    alias: 'h', type: Boolean
+  },
+  {
+    name: 'version', description: 'Display the version of this command',
+    alias: 'v', type: Boolean
+  },
+  {
+    name: 'appname', description: 'The application name to update config.xml',
+    alias: 'n', type: String
+  },
+  {
+    name: 'appversion', description: 'The application version to update config.xml',
+    alias: 'a', type: String
+  },
+  {
+    name: 'appid', description: 'The application id to update config.xml',
+    alias: 'i', type: String
+  }
+];
+
+const options = {
+  title: 'cordova-update-config',
+  description: "A command line tool to update cordova's config.xml, useful in continuous integration build environments.",
+  synopsis: [
+    '$ cordova-update-config --appname org.company.app --appversion 5.3.223 ...',
+    '$ cordova-update-config --help'
+  ],
+  footer: [
+    ''
+  ]
+};
+
+var cli = commandLineArgs(optionDefinitions);
+var args = cli.parse();
+
+if (args.help || Object.keys(args).length == 0) {
+	console.log(cli.getUsage(options));
+	return;
+}
+
+var cordova         = require('cordova-lib').cordova;
+var ConfigParser    = require('cordova-common').ConfigParser;
+var CordovaError    = require('cordova-common').CordovaError;
+var fs              = require('fs');
+var path            = require('path');
+
+if (args.version)
+{
+  var pkg = require('../package.json');
+  var cdvVer = require('cordova-lib/package').version;
+  console.log('cordova-update-config ' + pkg.version);
+  console.log('cordova ' + cdvVer);
+  return;
+}
+
+var projectRoot = cordova.findProjectRoot();
+if (!projectRoot) {
+  throw new CordovaError('Current working directory is not a Cordova-based project.');
+}
+
+var configPath = path.join(projectRoot, 'config.xml');
+var config = new ConfigParser(configPath);
+
+if (args.appid) {
+  config.setPackageName(args.appid);
+}
+
+if (args.appname) {
+  config.setName(args.appname);
+}
+
+if (args.appversion) {
+  config.setVersion(args.appversion);
+}
+
+config.write();
